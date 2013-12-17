1e72419dd09b8613d75a0bfa68d9520811bad542
g_rocket <g.rocket1@gmail.com>
2013-12-17 18:15:17 -0500

    add iterators and copy constructor to RNASequence

--- src/net/clonecomputers/lab/rna/RNASequence.java
+++ src/net/clonecomputers/lab/rna/RNASequence.java
@@ -1,5 +1,7 @@
 package net.clonecomputers.lab.rna;
 
+import java.util.*;
+
 import net.clonecomputers.lab.rna.d2.*;
 
 public class RNASequence {
@@ -15,4 +17,64 @@ public class RNASequence {
 		}
 	}
 	
+	public RNASequence(RNASequence s) {
+		pairs = Arrays.copyOf(s.pairs, s.pairs.length);
+		path = Arrays.copyOf(s.path, s.path.length);
+	}
+	
+	public Iterable<RNABasePair> getPairs() {
+		return new Iterable<RNABasePair>() {
+			
+			@Override
+			public Iterator<RNABasePair> iterator() {
+				return new Iterator<RNABasePair>() {
+					int i = 0;
+					
+					@Override
+					public boolean hasNext() {
+						return i < RNASequence.this.pairs.length;
+					}
+
+					@Override
+					public RNABasePair next() {
+						return RNASequence.this.pairs[i++];
+					}
+
+					@Override
+					public void remove() {
+						throw new UnsupportedOperationException();
+					}
+					
+				};
+			}
+		};
+	}
+	
+	public Iterable<Turn> getPath() {
+		return new Iterable<Turn>() {
+			
+			@Override
+			public Iterator<Turn> iterator() {
+				return new Iterator<Turn>() {
+					int i = 0;
+					
+					@Override
+					public boolean hasNext() {
+						return i < RNASequence.this.path.length;
+					}
+
+					@Override
+					public Turn next() {
+						return RNASequence.this.path[i++];
+					}
+
+					@Override
+					public void remove() {
+						throw new UnsupportedOperationException();
+					}
+					
+				};
+			}
+		};
+	}
 }
