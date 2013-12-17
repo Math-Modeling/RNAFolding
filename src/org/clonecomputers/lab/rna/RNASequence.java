22eebe366d1336e11ffe483cc8d77b16064f8cd9
g_rocket <g.rocket1@gmail.com>
2013-12-17 17:45:32 -0500

    finish direction stuff

--- src/org/clonecomputers/lab/rna/RNASequence.java
+++ src/org/clonecomputers/lab/rna/RNASequence.java
@@ -1,10 +1,14 @@
 package org.clonecomputers.lab.rna;
 
+import org.clonecomputers.lab.rna.d2.*;
+
 public class RNASequence {
 	private RNABasePair[] pairs;
+	private Turn[] path;
 	
 	public RNASequence(String sequence) {
 		pairs = new RNABasePair[sequence.length()];
+		path = new Turn[pairs.length];
 		int i = 0;
 		for(char c: sequence.toUpperCase().toCharArray()){
 			pairs[i++] = RNABasePair.valueOf(String.valueOf(c));
--- src/org/clonecomputers/lab/rna/d2/Direction.java
+++ src/org/clonecomputers/lab/rna/d2/Direction.java
@@ -1,8 +1,18 @@
 package org.clonecomputers.lab.rna.d2;
 
 public enum Direction {
-	NORTH,
-	EAST,
-	SOUTH,
-	WEST;
+	NORTH(0,1),
+	EAST(-1,0),
+	SOUTH(0,-1),
+	WEST(1,0);
+	
+	private final Point mod;
+	
+	private Direction(int x, int y) {
+		this.mod = new Point(x,y);
+	}
+	
+	private Point move(Point p) {
+		return new Point(p.x + mod.x, p.y + mod.y);
+	}
 }
