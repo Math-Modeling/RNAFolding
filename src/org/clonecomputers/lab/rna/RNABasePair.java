eb59a0d60a5de6d8a672d7decdcc2830dc28137b
g_rocket <g.rocket1@gmail.com>
2013-12-17 17:25:58 -0500

    start data structure

--- src/org/clonecomputers/lab/rna/RNABasePair.java
+++ src/org/clonecomputers/lab/rna/RNABasePair.java
@@ -1,5 +1,18 @@
 package org.clonecomputers.lab.rna;
 
-public class RNABasePair {
-
+public enum RNABasePair {
+	A("U"),
+	C("G"),
+	G("C"),
+	U("A");
+	
+	private final RNABasePair conjugate;
+	
+	private RNABasePair(String conjugateName) {
+		conjugate = RNABasePair.valueOf(conjugateName);
+	}
+	
+	private boolean bondsWith(RNABasePair pair) {
+		return pair.equals(conjugate);
+	}
 }
--- src/org/clonecomputers/lab/rna/RNASequence.java
+++ src/org/clonecomputers/lab/rna/RNASequence.java
@@ -1,5 +1,14 @@
 package org.clonecomputers.lab.rna;
 
 public class RNASequence {
-
+	private RNABasePair[] pairs;
+	
+	public RNASequence(String sequence) {
+		pairs = new RNABasePair[sequence.length()];
+		int i = 0;
+		for(char c: sequence.toUpperCase().toCharArray()){
+			pairs[i++] = RNABasePair.valueOf(String.valueOf(c));
+		}
+	}
+	
 }
