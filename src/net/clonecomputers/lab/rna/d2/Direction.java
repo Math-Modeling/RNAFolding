6864c2394f5fd51ea0d2d7252fa9cc64a89f35ea
g-rocket <g.rocket1@gmail.com>
2014-01-28 18:23:16 -0500

    proper encoding of unicode characters

--- src/net/clonecomputers/lab/rna/d2/Direction.java
+++ src/net/clonecomputers/lab/rna/d2/Direction.java
@@ -1,10 +1,10 @@
 package net.clonecomputers.lab.rna.d2;
 
 public enum Direction {
-	NORTH(0,1,"↑"),
-	EAST(-1,0,"→"),
-	SOUTH(0,-1,"↓"),
-	WEST(1,0,"←");
+	NORTH(0,1,"\u2191"),
+	EAST(-1,0,"\u2192"),
+	SOUTH(0,-1,"\u2193"),
+	WEST(1,0,"\u2190");
 	
 	private final Point mod;
 	private final String dir;
