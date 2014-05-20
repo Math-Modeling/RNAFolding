88f6c79c01803ee0dedb85409f11b0dc98252d8c
g_rocket <g.rocket1@gmail.com>
2014-05-19 21:19:53 -0400

    allow random rna sequences or passing sequences by commandline

--- src/net/clonecomputers/lab/rna/d2/folders/RandomFolder.java
+++ src/net/clonecomputers/lab/rna/d2/folders/RandomFolder.java
@@ -10,8 +10,23 @@ import net.clonecomputers.lab.rna.d2.*;
 public class RandomFolder extends RNAFolder {
 	
 	public static void main(String[] args) {
-		RNASequence sequence = new RNASequence(JOptionPane.showInputDialog("Input an RNA string")
-				.toUpperCase().replace('T', 'U')); // allow DNA strings too
+		RNASequence sequence;
+		if(args.length == 1 && args[0].equalsIgnoreCase("random")) {
+			int len = (int)(Math.random()*30+20);
+			StringBuilder sb = new StringBuilder();
+			RNABasePair[] pairs = RNABasePair.values();
+			for(int i = 0; i < len; i++) {
+				sb.append(pairs[(int)(Math.random()*pairs.length)].name());
+			}
+			final String seq = sb.toString();
+			System.out.println("sequence = "+seq);
+			sequence = new RNASequence(seq);
+		} else if(args.length == 1) {
+			sequence = new RNASequence(args[0].toUpperCase().replace('T', 'U')); // allow DNA strings too
+		} else {
+			sequence = new RNASequence(JOptionPane.showInputDialog("Input an RNA string")
+					.toUpperCase().replace('T', 'U')); // allow DNA strings too
+		}
 		new RandomFolder(sequence).fold();
 	}
 
